c60103e95a7287421c4a9dd3eec52ce48f057bc2
vinod123choudhari <114797469+vinod123choudhari@users.noreply.github.com>
2025-06-15 04:54:38 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" //  treat all js node as newer version
+
+// alert(3 + 3) // we are using nodejs, not a browser
+
+console.log(3 
+    +3
+) // code should be higher readability
+
+console.log("vinod choudhary")
+
+let number = 123
+let string = "vinod"
+let islogID = false
+let carname = null
+let Collegename;
+
+//datatype
+// number => range 2 power 53
+// bigInt
+//string = ""
+//boolean = true/false
+//null = standalone value
+//undefined
+//symbol = unique 
+
+//object
+
+console.log(typeof carname) // object
+console.log(typeof Collegename)// undefined 
