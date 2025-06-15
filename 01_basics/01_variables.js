5c7170b7ecddfaaad264b11449dd6d8d2f0d51f5
vinod123choudhari <114797469+vinod123choudhari@users.noreply.github.com>
2025-06-15 04:54:25 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountholder = "vinod kumar chaudhary"
+var City = "Dehradun"
+accountlocation ="dehradun"
+
+// const use not change after define
+
+//const accountId = 123
+
+console.log(accountId)
+/*
+perfer not use to var
+because of issue in block scope  and functional scope */
+
+
+console.table([accountId,accountholder,City,accountlocation])
