7ab04340898a9255f6525544b99c7e355886b88b
vinod123choudhari <114797469+vinod123choudhari@users.noreply.github.com>
2025-06-15 15:55:19 +0000

    conversion datatype

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,7 +1,7 @@
 let score = "33"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let numberIsconversion = Number(score)
 console.log(typeof numberIsconversion)
@@ -15,8 +15,31 @@ let booleannumber = 1
 
 let booleanIsnumber = Boolean(booleannumber)
 
-console.log(booleanIsnumber)
+// console.log(booleanIsnumber)
 
 // 1 => true , 0 => false
 //"" => false
 // "vinod" => true 
+
+//***********************operations********************//
+
+let value = 5
+let nagativevalue = -value
+
+// console.log(nagativevalue)
+
+// console.log("1" + 4)
+// console.log(4 + "2")
+// console.log(2 + 3 + "3")
+// console.log("4" + 4 +3)
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 +2
+
+let numberCounter = 100
+numberCounter++;
+console.log(numberCounter)
