d4dfd5f3b5b569b9943c98532bcae52b0c3f4abf
vinod123choudhari <114797469+vinod123choudhari@users.noreply.github.com>
2025-06-15 13:16:00 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,22 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let numberIsconversion = Number(score)
+console.log(typeof numberIsconversion)
+
+// "33" => 33
+//"33abc" => NaN
+//Ture =>1 , false => 0
+//null =>0
+// undefined => NaN
+let booleannumber = 1
+
+let booleanIsnumber = Boolean(booleannumber)
+
+console.log(booleanIsnumber)
+
+// 1 => true , 0 => false
+//"" => false
+// "vinod" => true 
