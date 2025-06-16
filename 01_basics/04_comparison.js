0e375bb90fc9a5800bc7933506d3489c0ce92e74
vinod123choudhari <114797469+vinod123choudhari@users.noreply.github.com>
2025-06-16 14:35:58 +0000

    04 comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// best comparion methods
+
+console.log(4 > 5)
+console.log( 5==5)
+
+// note use this methods 
+
+console.log("3" == 5)
+// console.log("03" = 3)
+
+
+//  ===  comparison with datatype
+
+console.log(5 === 5)
