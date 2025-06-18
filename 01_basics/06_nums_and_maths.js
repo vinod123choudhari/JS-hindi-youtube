c6791afa2a3ce3eec1e43a63c5cbf0382ec0fb1a
vinod123choudhari <114797469+vinod123choudhari@users.noreply.github.com>
2025-06-18 02:54:41 +0000

    06 nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,27 @@
+const score = 100
+
+// convert string
+
+// console.log(score.toString())
+
+// used fixed length
+
+// console.log(score.toFixed(2))
+
+//use spacialized hundreds 
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//+++++++++++++++++++++Maths+++++++++++++++++++++
+
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.6))
+console.log(Math.floor(4.6))
+console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() *(max- min +1)) + min)
